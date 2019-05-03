033cbe4cddaf25b8f7b88bb4f811a405d29ad4a8
Marco Mendão <mac.mendao@gmail.com>
2019-05-03 11:30:47 +0100

    a ines esqueceu-se das virgulas

--- scraper/model/entities.py
+++ scraper/model/entities.py
@@ -8,13 +8,13 @@ class Database:
         self.model = PonyDatabase(**db_params)
 
         class Author(self.model.Entity):
-            #id = PrimaryKey(int, auto=True),
-            name = Required(str, unique=True),
-            birthdate = Required(str),
+            id = PrimaryKey(int, auto=True)
+            name = Required(str, unique=True)
+            birthdate = Required(str)
             bio = Required(str)
 
         class Quote(self.model.Entity):
-            #id = PrimaryKey(int, auto=True),
-            text = Required(str, unique=True),
+            id = PrimaryKey(int, auto=True)
+            text = Required(str, unique=True)
             author_name = Required(str)
             # tags = Required(Json)
